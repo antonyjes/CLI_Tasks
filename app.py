b9b139f94c6ead51dadc46dc2d4bbb0985d488c5
antonyjes <72809149@continental.edu.pe>
2024-08-12 18:27:47 -0500

    fetching commands for the main app

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import click
-from crud import add_task
+from crud import add_task, list_tasks, update_task, delete_task
+from list_by_status import list_in_progress, list_todo, list_done
 
 @click.group()
 def cli():
     pass
 
-
+# Main CRUD commands
 cli.add_command(add_task)
+cli.add_command(list_tasks)
+cli.add_command(update_task)
+cli.add_command(delete_task)
+
+# Commands to list tasks by status
+cli.add_command(list_in_progress)
+cli.add_command(list_todo)
+cli.add_command(list_done)
 
 
 if __name__ == '__main__':
