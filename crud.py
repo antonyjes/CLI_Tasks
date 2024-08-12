775f6b7010acf99429448526dc2e39c8a50db9e3
antonyjes <72809149@continental.edu.pe>
2024-08-12 12:48:50 -0500

    list all tasks command

--- crud.py
+++ crud.py
@@ -3,6 +3,8 @@ from utils import load_json, save_json
 import uuid
 import datetime
 
+
+# Create a new task
 @click.command()
 @click.argument('name')
 def add_task(name):
@@ -21,3 +23,13 @@ def add_task(name):
     save_json("tasks.json", data)
 
     click.echo("Task added successfully!")
+
+
+# Read the list of tasks
+@click.command()
+def list_tasks():
+    data = load_json("tasks.json")
+
+    click.echo("ID | NAME | STATUS")
+    for task in data['tasks']:
+        click.echo(f"{task['id']} | {task['name']} | {task['status']}")
